fe378bbcb321cd6223b3cfda1322979d5b8a1fca
wellyct <wellyct@yahoo.com>
2023-09-11 16:20:54 +0700

    proyek-pertama done

---
+++ berkas/build_push_image.sh
@@ -0,0 +1,14 @@
+## menyimpan kode token akses ghcr.io ke variable
+echo "EXPORT WELLYCT_PAT = ##KODE_TOKEN_ACCESS##" >> /home/welly/.bashrc
+
+## login ghcr.io dengan menggunakan username dan token
+echo $WELLYCT_PAT | docker login ghcr.io -u wellyct --password-stdin
+
+## memberikan informasi image tag 
+docker tag item-app:v1 ghcr.io/wellyct/item-app:v1
+
+## mengirimkan image ke ghcr.io
+docker push ghcr.io/wellyct/item-app:v1
+
+## melihat informasi image yang sudah di upload
+docker inspect ghcr.io/wellyct/item-app:v1
